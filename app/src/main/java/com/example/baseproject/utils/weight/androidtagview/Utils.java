0b771a41fba799335d7b630547ea9b9d1ed3591d
余沃铨 <841453858@qq.com>
2019-02-25 15:00:37 +0800

    添加自定义控件

---
+++ app/src/main/java/com/example/baseproject/utils/weight/androidtagview/Utils.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 lujun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.baseproject.utils.weight.androidtagview;
+
+import android.content.Context;
+import android.graphics.Color;
+
+/**
+ * Author: lujun(http://blog.lujun.co)
+ * Date: 2016-12-7 21:53
+ */
+
+public class Utils {
+
+    public static float dp2px(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return dp * scale + 0.5f;
+    }
+
+    public static float sp2px(Context context, float sp) {
+        final float scale = context.getResources().getDisplayMetrics().scaledDensity;
+        return sp * scale;
+    }
+
+    /**
+     *Manipulate Birghtness of color
+     *
+     * @param color in int, brightness factor
+     *
+     * @return int
+     */
+    public static int manipulateColorBrigthness(int color, float factor) {
+        int a = Color.alpha(color);
+        int r = Math.round(Color.red(color) * factor);
+        int g = Math.round(Color.green(color) * factor);
+        int b = Math.round(Color.blue(color) * factor);
+        return Color.argb(a,
+                Math.min(r,255),
+                Math.min(g,255),
+                Math.min(b,255));
+    }
+}
